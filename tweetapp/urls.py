a26909f26ba4325d5846242e931b5d60b717025e
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-13 01:03:38 +0600

    added querysets

--- tweetapp/urls.py
+++ tweetapp/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('',tweet_list_view,name="list"),
-    path('1/',tweet_detail_view,name="detail")
+    path('detail',tweet_detail_view,name="detail")
 
 ]
--- tweetapp/views.py
+++ tweetapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def tweet_detail_view(request,id=1):
-    return render(request,"detail_view.html")
+    obj=Tweet.objects.get(id=id)
+
+    context={
+        "object":obj
+    }
+    return render(request,"detail_view.html",context)
 
 def tweet_list_view(request):
-    return render(request,"list_view.html")
+    queryset=Tweet.objects.all()
+    context={
+        "object_list":queryset
+    }
+    print(queryset)
+    return render(request,"list_view.html",context)
+
