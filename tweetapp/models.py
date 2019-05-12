30e2b2d35fc6c99cadf302c187171628d5b94f2f
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-12 23:45:17 +0600

    added user as foreign key

--- tweetapp/models.py
+++ tweetapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Tweet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , default=1,on_delete=models.DO_NOTHING)
     content = models.CharField(max_length=180)
     timestamp = models.DateTimeField(auto_now_add=True)
+
 
 
     def __str__(self):
