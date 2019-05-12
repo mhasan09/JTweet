4b704e303c0cb47017c954c7e3445bd412471273
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-12 14:30:33 +0600

    added models, registered admin section

--- tweetapp/models.py
+++ tweetapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    content = models.CharField(max_length=180)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return str(self.content)
