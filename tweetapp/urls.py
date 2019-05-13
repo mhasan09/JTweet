1a0a2e9a486d3644ec5ce05bfa13a53f75df2b47
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-13 14:58:11 +0600

    Revert "added dynamic based views"
    This reverts commit 497253d0eba3ad93672412db1008f78cd75a931b.

--- tweetapp/urls.py
+++ tweetapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 app_name='tweetapp'
 urlpatterns = [
-    path('', views.TweetListView.as_view(),name="detail"),
-    path('<int:pk>/', views.TweetDetailView.as_view(),name="list"),
 
+    path('', views.TweetDetailView.as_view(),name="list"),
+    path('1/', views.TweetListView.as_view(),name="detail"),
 
 
 
