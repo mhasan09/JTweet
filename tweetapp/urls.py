a6c24ec15d460187fc28a3192796f69fb6531f5d
Mahabubul Hasan <shourav6688@gmail.com>
2019-05-21 14:00:52 +0600

    added Redirect view

--- tweetapp/urls.py
+++ tweetapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import TweetCreateView,TweetListView,TweetDetailView,TweetUpdateView, TweetDeleteView
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import RedirectView
 app_name='tweetapp'
 urlpatterns = [
-    path('', TweetListView.as_view(),name="list"),
+    path('',RedirectView.as_view(url="/")),
+    path('search/', TweetListView.as_view(),name="list"),
     path('<int:pk>/', TweetDetailView.as_view(),name="detail"),
     path('create/', login_required(TweetCreateView.as_view()),name="create"),
     path('<int:pk>/update/', login_required(TweetUpdateView.as_view()),name="update"),
